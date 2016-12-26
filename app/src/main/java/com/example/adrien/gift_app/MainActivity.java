84e94664e3a28b212a4ddc0d88296e52cbd2b733
Mumu Des Bois <mula.adri@gmail.com>
2016-12-26 23:52:09 +0100

    Finish design navbar

--- app/src/main/java/com/example/adrien/gift_app/MainActivity.java
+++ app/src/main/java/com/example/adrien/gift_app/MainActivity.java
@@ -15,8 +15,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        BottomBar mBottomBar = (BottomBar) findViewById(R.id.bottomBar);
-
         BottomBar bottomBar = (BottomBar) findViewById(R.id.bottomBar);
         bottomBar.setOnTabSelectListener(new OnTabSelectListener() {
             @Override
