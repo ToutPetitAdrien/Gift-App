8c1a74511758a089d98e9444e12be1f17503e280
Mumu Des Bois <mula.adri@gmail.com>
2016-12-26 23:14:18 +0100

    Bottom navbar v2

--- app/src/main/java/com/example/adrien/gift_app/MainActivity.java
+++ app/src/main/java/com/example/adrien/gift_app/MainActivity.java
@@ -1,54 +1,29 @@
 package com.example.adrien.gift_app;
 
+
 import android.support.annotation.IdRes;
-import android.support.design.widget.CoordinatorLayout;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-
 import com.roughike.bottombar.BottomBar;
-import com.roughike.bottombar.OnMenuTabSelectedListener;
+import com.roughike.bottombar.OnTabSelectListener;
+
 
 public class MainActivity extends AppCompatActivity {
-
-    private CoordinatorLayout coordinatorLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        coordinatorLayout = (CoordinatorLayout)findViewById(R.id.activity_main);
+        BottomBar mBottomBar = (BottomBar) findViewById(R.id.bottomBar);
 
-        BottomBar bottomBar = BottomBar.attach(this, savedInstanceState);
-        bottomBar.setItemsFromMenu(R.menu.three_buttons_menu, new OnMenuTabSelectedListener() {
+        BottomBar bottomBar = (BottomBar) findViewById(R.id.bottomBar);
+        bottomBar.setOnTabSelectListener(new OnTabSelectListener() {
             @Override
-            public void onMenuItemSelected(@IdRes int menuItemId) {
-                switch (menuItemId) {
-                    case R.id.recent_item:
-                        Snackbar.make(coordinatorLayout, "Recent Item Selected", Snackbar.LENGTH_LONG).show();
-                        break;
-                    case R.id.favorite_item:
-                        Snackbar.make(coordinatorLayout, "Favorite Item Selected", Snackbar.LENGTH_LONG).show();
-                        break;
-                    case R.id.location_item:
-                        Snackbar.make(coordinatorLayout, "Location Item Selected", Snackbar.LENGTH_LONG).show();
-                        break;
-                }
+            public void onTabSelected(@IdRes int tabId) {
+
             }
         });
 
-        // Set the color for the active tab. Ignored on mobile when there are more than three tabs.
-        bottomBar.setActiveTabColor("#C2185B");
-
-        // Use the dark theme. Ignored on mobile when there are more than three tabs.
-        //bottomBar.useDarkTheme(true);
-
-        // Use custom text appearance in tab titles.
-        //bottomBar.setTextAppearance(R.style.MyTextAppearance);
-
-        // Use custom typeface that's located at the "/src/main/assets" directory. If using with
-        // custom text appearance, set the text appearance first.
-        //bottomBar.setTypeFace("MyFont.ttf");
     }
 }
