3bb5493b208736c4ab8a8450d292ba66287954ae
Mumu Des Bois <mula.adri@gmail.com>
2016-12-26 12:42:05 +0100

    Bottom navigation bar template

--- app/src/main/java/com/example/adrien/gift_app/MainActivity.java
+++ app/src/main/java/com/example/adrien/gift_app/MainActivity.java
@@ -1,13 +1,54 @@
 package com.example.adrien.gift_app;
 
+import android.support.annotation.IdRes;
+import android.support.design.widget.CoordinatorLayout;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.roughike.bottombar.BottomBar;
+import com.roughike.bottombar.OnMenuTabSelectedListener;
+
 public class MainActivity extends AppCompatActivity {
+
+    private CoordinatorLayout coordinatorLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        coordinatorLayout = (CoordinatorLayout)findViewById(R.id.activity_main);
+
+        BottomBar bottomBar = BottomBar.attach(this, savedInstanceState);
+        bottomBar.setItemsFromMenu(R.menu.three_buttons_menu, new OnMenuTabSelectedListener() {
+            @Override
+            public void onMenuItemSelected(@IdRes int menuItemId) {
+                switch (menuItemId) {
+                    case R.id.recent_item:
+                        Snackbar.make(coordinatorLayout, "Recent Item Selected", Snackbar.LENGTH_LONG).show();
+                        break;
+                    case R.id.favorite_item:
+                        Snackbar.make(coordinatorLayout, "Favorite Item Selected", Snackbar.LENGTH_LONG).show();
+                        break;
+                    case R.id.location_item:
+                        Snackbar.make(coordinatorLayout, "Location Item Selected", Snackbar.LENGTH_LONG).show();
+                        break;
+                }
+            }
+        });
+
+        // Set the color for the active tab. Ignored on mobile when there are more than three tabs.
+        bottomBar.setActiveTabColor("#C2185B");
+
+        // Use the dark theme. Ignored on mobile when there are more than three tabs.
+        //bottomBar.useDarkTheme(true);
+
+        // Use custom text appearance in tab titles.
+        //bottomBar.setTextAppearance(R.style.MyTextAppearance);
+
+        // Use custom typeface that's located at the "/src/main/assets" directory. If using with
+        // custom text appearance, set the text appearance first.
+        //bottomBar.setTypeFace("MyFont.ttf");
     }
 }
