b19ad08ba6488f4500676df3bc556d0d842e7e50
Mumu Des Bois <mula.adri@gmail.com>
2016-12-30 13:38:51 +0100

    form ideas part1

--- app/src/main/java/com/example/adrien/gift_app/MainActivity.java
+++ app/src/main/java/com/example/adrien/gift_app/MainActivity.java
@@ -3,15 +3,13 @@ package com.example.adrien.gift_app;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
-
+import android.widget.EditText;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
-
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
-import java.net.URI;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -22,12 +20,14 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        EditText textTitle = (EditText)findViewById(R.id.editText_title);
+
+
         FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
         mDatabase = FirebaseDatabase.getInstance().getReference();
         Log.d("MainActivity","hello: "+ user.getUid());
 
         Idea idea = new Idea();
-        idea.setCreatedBy("Adrien");
         idea.setTitle("PS4");
         idea.setPrice(400);
         idea.setDescription("Un kdo de l'enfer !");
