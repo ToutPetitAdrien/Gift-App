572518c3702b2ec03b293c3b967320c60c6e406b
Mumu Des Bois <mula.adri@gmail.com>
2017-01-07 20:07:52 +0100

    finish design fragment event

--- app/src/main/java/com/example/adrien/gift_app/EventsFragment.java
+++ app/src/main/java/com/example/adrien/gift_app/EventsFragment.java
@@ -1,13 +1,10 @@
 package com.example.adrien.gift_app;
 
-import android.content.Context;
-import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
-import android.text.Editable;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
