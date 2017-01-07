ab264d477a581fd1a299b97cdda3836d35ee3beb
Mumu Des Bois <mula.adri@gmail.com>
2017-01-07 10:31:47 +0100

    commit

--- app/src/main/java/com/example/adrien/gift_app/MainActivity.java
+++ app/src/main/java/com/example/adrien/gift_app/MainActivity.java
@@ -7,18 +7,11 @@ import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
-import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
-
-import java.text.DateFormat;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
-
 
 public class MainActivity extends AppCompatActivity {
 
