7497eee57539e643ec5d26a38551815079bd4c21
Mumu Des Bois <mula.adri@gmail.com>
2017-02-14 13:59:44 +0100

    uploading image on firebase

--- app/src/main/java/com/example/adrien/gift_app/IdeasFormFragment.java
+++ app/src/main/java/com/example/adrien/gift_app/IdeasFormFragment.java
@@ -1,6 +1,10 @@
 package com.example.adrien.gift_app;
 
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.util.Log;
@@ -14,6 +18,8 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
@@ -21,11 +27,20 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+import static android.app.Activity.RESULT_OK;
+
 
 public class IdeasFormFragment extends Fragment {
 
     private DatabaseReference mDatabase;
     private FirebaseUser user;
+    private FirebaseStorage storage;
+    private static final int GALLERY_INTENT = 2;
+    private ProgressDialog progressDialog;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -36,8 +51,31 @@ public class IdeasFormFragment extends Fragment {
     }
 
     @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        StorageReference storageRef = storage.getReferenceFromUrl("gs://gift-app-fd6d4.appspot.com");
+
+        if(requestCode == GALLERY_INTENT && resultCode == RESULT_OK){
+
+            progressDialog.setMessage("Uploading ...");
+            progressDialog.show();
+            Uri uri = data.getData();
+            StorageReference filepath = storageRef.child("Photos").child(uri.getLastPathSegment());
+            filepath.putFile(uri).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                @Override
+                public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                    Toast.makeText(getActivity(), "Upload Done.", Toast.LENGTH_SHORT).show();
+                    progressDialog.dismiss();
+                }
+            });
+        }
+    }
+
+    @Override
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
 
+        storage = FirebaseStorage.getInstance();
         user = FirebaseAuth.getInstance().getCurrentUser();
         mDatabase = FirebaseDatabase.getInstance().getReference();
         final EditText textTitle = (EditText)view.findViewById(R.id.form_title);
@@ -48,7 +86,9 @@ public class IdeasFormFragment extends Fragment {
         final AutoCompleteTextView textForWhen = (AutoCompleteTextView)view.findViewById(R.id.form_when);
         final TextView submit_button = (TextView) view.findViewById(R.id.addbutton2_text);
 
-        Log.d("IdeasFormFragment", "Je suis dans le fragment des ajouts  d'idées");
+        progressDialog = new ProgressDialog(getContext());
+
+
 
         // Suggestion event on textForWhen field
 
@@ -92,5 +132,17 @@ public class IdeasFormFragment extends Fragment {
                 Toast.makeText(getActivity(),"Idées ajoutée", Toast.LENGTH_SHORT).show();
             }
         });
+
+        textPhoto.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(Intent.ACTION_PICK);
+                intent.setType("image/*");
+                startActivityForResult(intent, GALLERY_INTENT);
+            }
+        });
+
+
     }
 }
